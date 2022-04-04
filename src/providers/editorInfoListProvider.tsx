17ff60ffa404d65aaaffe30853d339f8be2e32c0
ryota-09 <n.ryo950911@gmail.com>
2022-04-04 12:19:04 +0900

    editorContext作成

--- src/providers/editorInfoListProvider.tsx
+++ src/providers/editorInfoListProvider.tsx
@@ -1,11 +1,50 @@
-import { Dispatch } from "react";
+import { createContext, Dispatch, ReactNode, useReducer } from "react";
+import { EditorInfoList } from "../types/editorInfoList";
 
 export type EditorInfoListContextType = {
   globalState: State;
   setGlobalState: Dispatch<Action>;
-}
+};
+
+export const EditorInfoListContext = createContext(
+  {} as EditorInfoListContextType
+);
 
 type State = {
   currentUser: null;
-  editorInfoList: 
-}
+  editorInfoList: Array<EditorInfoList>;
+};
+
+type Action = {
+  type: "CHANGE_VALUE" | "SET_EDITORINFOLIST";
+  payload: {};
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+const initialState: State = {
+  currentUser: null,
+  editorInfoList: [],
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "CHANGE_VALUE":
+    default:
+      return state;
+  }
+};
+
+export const EditorInfoListProvider = (props: Props) => {
+  const { children } = props;
+  const [globalState, setGlobalState] = useReducer(reducer, initialState);
+  return (
+    <>
+      <EditorInfoListContext.Provider value={{ globalState, setGlobalState }}>
+        {children}
+      </EditorInfoListContext.Provider>
+    </>
+  );
+};
