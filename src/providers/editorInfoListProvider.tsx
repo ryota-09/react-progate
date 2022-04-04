e4b9fa1343c3d54b1b4ae7734b65144ac54bbf35
ryota-09 <n.ryo950911@gmail.com>
2022-04-04 12:00:48 +0900

    EditorInfoListの型を作成

---
+++ src/providers/editorInfoListProvider.tsx
@@ -0,0 +1,11 @@
+import { Dispatch } from "react";
+
+export type EditorInfoListContextType = {
+  globalState: State;
+  setGlobalState: Dispatch<Action>;
+}
+
+type State = {
+  currentUser: null;
+  editorInfoList: 
+}
---
+++ src/types/editorInfoList.ts
@@ -0,0 +1,5 @@
+export type EditorInfoList = {
+  name: string;
+  language: string;
+  value: string;
+}
