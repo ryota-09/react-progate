c177972c73a40e347a37062bb6e7eb2640bf1d29
ryota-09 <n.ryo950911@gmail.com>
2022-04-09 13:59:28 +0900

    btn大きくした

--- src/components/pages/organisms/LangList.tsx
+++ src/components/pages/organisms/LangList.tsx
@@ -20,13 +20,13 @@ export const LangList = () => {
     <div className="container">
     <div className="row">
       <div className="col s3 offset-s1 ">
-        <button className="btn teal accent-4" onClick={beginnerButton}>Beginner</button>
+        <button className="btn-large teal accent-4" onClick={beginnerButton}>Beginner</button>
       </div>
       <div className="col s3 offset-s1">
-        <button className="btn orange darken-1" onClick={htmlButton}>HTML</button>
+        <button className="btn-large orange darken-1" onClick={htmlButton}>HTML</button>
       </div>
       <div className="col s3 offset-s1">
-        <button className="btn blue darken-1" onClick={typescriptButton}>TypeScript</button>
+        <button className="btn-large blue darken-1" onClick={typescriptButton}>TypeScript</button>
       </div>
     </div>
     </div>
